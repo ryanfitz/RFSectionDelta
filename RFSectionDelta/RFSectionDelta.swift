6d97895fded5765ea35ec72a7c4d105fbc88aeaa
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2015-08-13 08:41:45 -0400

    removing unused import

--- RFSectionDelta/RFSectionDelta.swift
+++ RFSectionDelta/RFSectionDelta.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 ryanfitz. All rights reserved.
 //
 
-import Foundation
-
 public struct MovedIndex : Equatable, Printable {
     public let oldIndex : Int
     public let newIndex : Int
